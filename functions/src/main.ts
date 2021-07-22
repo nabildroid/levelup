3c15a5d87c705338a7e32aaef0c5a6de7709c24e
nabil <pni20156789@gmail.com>
2021-07-22 20:26:22 +0000

    locally test pubsub and functions

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,15 +1,21 @@
 import { PubsubTopics } from "./types/general";
 import * as functions from "firebase-functions";
-
+import {PubSub} from "@google-cloud/pubsub"
 console.log(PubsubTopics.NOTION_NEW_CONTENT);
 
-export const hello_world = functions.https.onRequest((req,res)=>{
+export const hello_world = functions.pubsub.topic("test").onPublish(()=>{
+    console.log("subscriber");
+    return Promise.resolve("sss");
+})
 
-    res.send("hello world");
 
+const bus =new PubSub();
+
+export const httpRequest = functions.https.onRequest(async (req,res) =>{
+    await bus.topic("test").publish(Buffer.from("hello world"));
+
+    res.send("done");
 });
-
-// functions.pubsub.topic(PubsubTopics.NOTION_NEW_CONTENT).onPublish()
 
 export interface IHELLO_WORLD {
     isOdd: (a: number) => boolean
