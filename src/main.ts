66f5c566088425655bb01c78ba0d0ded51b167a2
nabil <pni20156789@gmail.com>
2021-07-22 15:27:13 +0000

    setup testing

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
-console.log("hello world")
+
+
+export interface IHELLO_WORLD {
+    isOdd: (a: number) => boolean
+}
+
+export  class HelloWorld implements IHELLO_WORLD {
+
+    isOdd(a: number) {
+        return a % 2 != 0;
+    }
+
+}
