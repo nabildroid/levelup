908166dd5f50e27d8e039332ae90b18b63dce66b
nabil <pni20156789@gmail.com>
2021-07-26 17:51:58 +0000

    implement findWhere

--- functions/src/functions/findWhere.ts
+++ functions/src/functions/findWhere.ts
@@ -1,19 +1,45 @@
 import * as functions from "firebase-functions";
-import { Message } from "firebase-functions/lib/providers/pubsub";
-import { firestore } from "..";
+import { firestore, pubsub } from "..";
 import PubSubConnector from "../connectors/pubsub";
-import { PubsubPublishUpdateAttribute } from "../types/general";
-import { Task } from "../types/task";
+import { PubsubInsertTaskAttributes } from "../types/pubsub";
+import { NTID, StoredTask, Task } from "../types/task";
 
+const { INSERT_TASK } = PubSubConnector.pubsubTopics;
 
-
-const NOTION_NEW_CONTENT = PubSubConnector.pubsubTopics.NOTION_NEW_CONTENT;
-
-export default functions.pubsub.topic(NOTION_NEW_CONTENT)
-    .onPublish((message, context) => {
-        const attribute = message.attributes as PubsubPublishUpdateAttribute;
+export default functions.pubsub
+    .topic(INSERT_TASK)
+    .onPublish(async (message, context) => {
+        const attribute = message.attributes as PubsubInsertTaskAttributes;
         const data = message.json() as Task;
 
+        const storedTask = await getStoredTask(data.id);
 
+        if (!storedTask) {
+            pubsub.detectedEventType(data, {
+                source: attribute.source,
+                type: "new"
+            })
+        } else {
+            if (storedTask.completed && !data.done) {
+                pubsub.validateTask(data, attribute.source);
+
+            } else {
+                pubsub.detectedEventType(data, {
+                    source: attribute.source,
+                    type: "update"
+                })
+            }
+        }
     });
 
+
+
+
+
+const getStoredTask = async (id: NTID): Promise<StoredTask | undefined> => {
+    const ref = firestore.collection("/tasks")
+        .where("id", "array-contains-any", id).limit(1);
+    const query = await ref.get();
+
+    return query.docs[0]?.data() as StoredTask;
+}
