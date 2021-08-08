0b845820fabf92fb9a3c425513eabfa6c41c8d2e
nabil <pni20156789@gmail.com>
2021-08-08 22:10:07 +0000

    add scheduler to cloudbuild

--- scripts/setupFunctions.sh
+++ scripts/setupFunctions.sh
@@ -19,45 +19,56 @@ function fnc(){
     if [[ "$config" != "null" ]]; then
         state=$(echo "$config" | jq ".state" | xargs)
         # make the function private
-        if [[ $state == "private" ]]; then
-            echo "$1" | xargs -I {} gcloud functions remove-iam-policy-binding {} --member=allUsers --role=roles/cloudfunctions.invoker
-        fi
-
-        topic=$(echo "$config" | jq ".topic")
-        topic=$(echo $topic | xargs printf)
-        if [[ "$topic" != "null" ]]; then
-            exists=$(echo "$topic" | xargs -I {} sh -c "gcloud pubsub topics list | grep {}" )
-
-            # create topic
-            if [[ -z "$exists" ]]; then
-                printf $topic | xargs -I {} gcloud pubsub topics create {}
+            if [[ $state == "private" ]]; then
+                echo "$1" | xargs -I {} gcloud functions remove-iam-policy-binding {} --member=allUsers --role=roles/cloudfunctions.invoker
             fi
+        
+        endpoint=$(fncEndpoint $1)
+        
+        # create topics and subscriptions
+            topic=$(echo "$config" | jq ".topic")
+            topic=$(echo $topic | xargs printf)
+            if [[ "$topic" != "null" ]]; then
+                exists=$(echo "$topic" | xargs -I {} sh -c "gcloud pubsub topics list | grep {}" )
 
-            # create subscription
-            filter=$(echo "$config" | jq ".filter")
-            if [[  $filter != "null" ]]; then
-                filter=$(echo "--message-filter='$filter'")
+                # create topic
+                if [[ -z "$exists" ]]; then
+                    printf $topic | xargs -I {} gcloud pubsub topics create {}
+                fi
+
+                # create subscription
+                filter=$(echo "$config" | jq ".filter")
+                if [[  $filter != "null" ]]; then
+                    filter=$(echo "--message-filter='$filter'")
+                fi
+
+                if [[  $filter == "null" ]]; then
+                    filter=""
+                fi
+                subscription_id="$1_$topic"
+                
+
+                subExists=$(echo $subscription_id | xargs -I {} gcloud pubsub subscriptions describe {} 2>/dev/null)
+
+                if [[ -n $subExists ]]; then 
+                
+                    # todo update pubsub
+                    # echo $filter | xargs -I {} echo "gcloud pubsub subscriptions update $subscription_id ---push-endpoint=$endpoint {}"
+                    echo $subscription_id | xargs -I {} gcloud pubsub subscriptions delete {}
+                fi
+
+                echo $filter | xargs -I {} -t sh -c "gcloud pubsub subscriptions create $subscription_id --topic=$topic --push-endpoint=$endpoint --labels=type=deployment {}"
+
             fi
-
-            if [[  $filter == "null" ]]; then
-                filter=""
+        
+        # create cloud scheduler
+            runEvery=$(echo "$config" | jq ".runEvery")
+            if [[ "$runEvery" != "null" ]]; then
+                schedulerName="runner_$1"
+                echo $schedulerName | xargs -I {} gcloud scheduler jobs delete {} --quiet
+                
+                echo $endpoint | xargs -I {} -t sh -c   "gcloud scheduler jobs create http $schedulerName --schedule $runEvery --uri {} "
             fi
-            subscription_id="$1_$topic"
-            
-            endpoint=$(fncEndpoint $1)
-
-            subExists=$(echo $subscription_id | xargs -I {} gcloud pubsub subscriptions describe {} 2>/dev/null)
-
-            if [[ -n $subExists ]]; then 
-            
-                # todo update pubsub
-                # echo $filter | xargs -I {} echo "gcloud pubsub subscriptions update $subscription_id ---push-endpoint=$endpoint {}"
-                echo $subscription_id | xargs -I {} gcloud pubsub subscriptions delete {}
-            fi
-
-            echo $filter | xargs -I {} -t sh -c "gcloud pubsub subscriptions create $subscription_id --topic=$topic --push-endpoint=$endpoint --labels=type=deployment {}"
-
-        fi
     fi
 }
 
